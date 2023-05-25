9c8edbd34ca5e6e3326f2eae91ac8479688fa09d
Romeli9 <119035936+Romeli9@users.noreply.github.com>
2023-05-25 18:36:24 +0700

    Update database.py

--- database.py
+++ database.py
@@ -15,6 +15,6 @@ class Students(db.Model):
         return f'<Student {self.full_name}>'
 
 def initialize_database(app):
-    db.init_app(app)
+#    db.init_app(app)
     with app.app_context():
         db.create_all()
